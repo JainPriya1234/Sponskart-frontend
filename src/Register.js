27afb2ab9511fa96511d6a3eb15847d914693eca
JainPriya1234 <priya.jain11302@gmail.com>
2023-08-01 23:45:07 +0530

    css added

--- src/Register.js
+++ src/Register.js
@@ -1,52 +1,66 @@
-import React, { useState } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
-import './App.css';
+  import React, { useState } from "react";
+  import axios from "axios";
+  import { Link } from "react-router-dom";
+  import './App.css';
 
-const Register = () => {
-  const [formData, setFormData] = useState({
-    firstname: "",
-    lastname: "",
-    username: "",
-    phonenumber: "",
-    email: "",
-    password: "",
-});
+  const Register = () => {
+    const [formData, setFormData] = useState({
+      firstname: "",
+      lastname: "",
+      username: "",
+      phonenumber: "",
+      email: "",
+      password: "",
+  });
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+    const handleChange = (e) => {
+      setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      try {
+        await axios.post('https://sponskart-hkgd.onrender.com/register', formData)
+        .then((res)=>{
+          console.log(res.data);
+        })
+        .catch((err)=>{
+          window.alert(err);
+          console.log(err);
+        })
+      } 
+      catch (error) {
+        console.error(error);
+      }
+    };
+
+    return (
+      <form onSubmit={handleSubmit}>
+        <h1>Sponskart</h1>
+        <h3 style={{ textAlign: "center" }}>It's Free to Sign Up and Get Started.</h3>
+        <p style={{ textAlign: "center" , color:"#676767"  }}>
+        Already have an account?{" "}
+        <Link to="/signin">Sign in</Link>
+      </p>      
+        <input type="text" name="firstname" placeholder="First Name" onChange={handleChange} required />
+        <input type="text" name="lastname" placeholder="Last Name" onChange={handleChange} required />
+        <input type="text" name="username" placeholder="Username" onChange={handleChange} required />
+        <input type="tel" name="phonenumber" placeholder="Phone Number" onChange={handleChange} required />
+        <input type="email" name="email" placeholder="Email" onChange={handleChange} required />
+        <input type="password" name="password" placeholder="Password" onChange={handleChange} required />
+        <p>I want to start as:</p><label>
+        <input type="radio" name="startAs" value="a" onChange={handleChange} required /> Brand 
+      </label>
+      <label>
+        <input type="radio" name="startAs" value="b" onChange={handleChange} required /> Influencer
+      </label>
+        <label>
+        <input type="radio" name="startAs" value="c" onChange={handleChange} required /> Event Organiser
+      </label>
+        <button type="submit">Register</button>
+      
+      </form>
+    );
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      await axios.post('http://localhost:3000/register', formData)
-      .then((res)=>{
-        console.log(res.data);
-      })
-      .catch((err)=>{
-        window.alert(err);
-        console.log(err);
-      })
-    } 
-    catch (error) {
-      console.error(error);
-    }
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" name="firstname" placeholder="First Name" onChange={handleChange} required />
-      <input type="text" name="lastname" placeholder="Last Name" onChange={handleChange} required />
-      <input type="text" name="username" placeholder="Username" onChange={handleChange} required />
-      <input type="tel" name="phonenumber" placeholder="Phone Number" onChange={handleChange} required />
-      <input type="email" name="email" placeholder="Email" onChange={handleChange} required />
-      <input type="password" name="password" placeholder="Password" onChange={handleChange} required />
-      <button type="submit">Register</button>
-      <Link to="/signin">Already have an account? Sign in</Link>
-    
-    </form>
-  );
-};
-
-export default Register;
+  export default Register;
--- src/Signin.js
+++ src/Signin.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 import './App.css';
 
 const Signin = () => {
@@ -15,7 +16,7 @@ const Signin = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('http://localhost:3000/signin', formData)
+      await axios.post('https://sponskart-hkgd.onrender.com/signin', formData)
       .then((res)=>{
         console.log(res.data);
       })
@@ -30,6 +31,12 @@ const Signin = () => {
 
   return (
     <form onSubmit={handleSubmit}>
+      <h1>Sponskart</h1>
+        <h3 style={{ textAlign: "center" }}>It's Free to Sign Up and Get Started.</h3>
+        <p style={{ textAlign: "center" , color:"#676767"  }}>
+        Don’t have an account?{" "}
+        <Link to="/">Register</Link>
+      </p>  
       <input type="email" name="email" placeholder="Email" onChange={handleChange} required />
       <input type="password" name="password" placeholder="Password" onChange={handleChange} required />
       <button type="submit">Login</button>
